51e7db0706fd022f1092eab8b4e4ff19ca6fdd3e
Saket <saketthakare@gmail.com>
2018-09-08 02:45:59 -0700

    Added Testcase and removed error
    + Added 5 test scenarios for Type 2 machine
    + Modified turn crank condition to check number of quarters greater than or equal to 2
    + Corrected typo in Type 2 machine's class.

--- lab-1/gumball-java-typical/GumballMachineType2.java
+++ lab-1/gumball-java-typical/GumballMachineType2.java
@@ -1,27 +1,27 @@
 public class GumballMachineType2 implements CrankMachine {
     private int num_gumballs;
-    private int quartar_count;
+    private int quarter_count;
 
     public GumballMachineType2(int size) {
         this.num_gumballs = size;
-        this.quartar_count = 0;
+        this.quarter_count = 0;
     }
 
     public void insertCoin(int coin) {
         if (coin == 25)
-            this.quartar_count++;
+            this.quarter_count++;
     }
 
     public void turnCrank() {
-        if (this.quartar_count == 2) {
+        if (this.quarter_count >= 2) {
             if (this.num_gumballs > 0) {
                 this.num_gumballs--;
-                this.quartar_count = 0;
+                this.quarter_count = 0;
                 System.out.println("Thanks for your quarter.  Gumball Ejected!");
             } else {
                 System.out.println("No More Gumballs!  Sorry, can't return your quarter.");
             }
-        } else if (this.quartar_count == 1) {
+        } else if (this.quarter_count == 1) {
             System.out.println("Please insert one more quarter");
         } else {
             System.out.println("Please insert two quarters");
--- lab-1/gumball-java-typical/Main.java
+++ lab-1/gumball-java-typical/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
 		gumballMachine.insertCoin( 25 );
 		gumballMachine.turnCrank();
-
 		System.out.println(gumballMachine);
 
 		gumballMachine.insertCoin( 25 );
@@ -19,5 +18,39 @@ public class Main {
 
 		System.out.println(gumballMachine);
 
-	}
+
+		GumballMachineType2 gbm2 = new GumballMachineType2(3);
+
+        System.out.println("\nScenario 1 where only 25 cents are inserted");
+		gbm2.insertCoin( 25 );
+        gbm2.turnCrank();
+
+        System.out.println("\nScenario 2 where remaining 25 cents are inserted");
+        gbm2.insertCoin( 25 );
+        gbm2.turnCrank();
+
+        System.out.println("\nScenario 3 where remaining coins other than quarter are inserted");
+        gbm2.insertCoin( 5 );
+        gbm2.turnCrank();
+        gbm2.insertCoin( 50 );
+        gbm2.turnCrank();
+
+        System.out.println("\nScenario 4 where total amount is more than 50.");
+        gbm2.insertCoin( 25 );
+        gbm2.insertCoin( 25 );
+        gbm2.insertCoin( 25 );
+        gbm2.insertCoin( 25 );
+        gbm2.turnCrank();
+
+        System.out.println("\nScenario 5 where all the Gumballs are used.");
+        gbm2.insertCoin( 25 );
+        gbm2.insertCoin( 25 );
+        gbm2.turnCrank();
+        gbm2.insertCoin( 25 );
+        gbm2.insertCoin( 25 );
+        gbm2.turnCrank();
+
+
+
+    }
 }
