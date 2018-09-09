40e3025a82787065559e156ae7b3c3f109ff5195
Saket <saketthakare@gmail.com>
2018-09-08 18:26:44 -0700

    Added Testcases for Gumball machine Type 3

--- lab-1/gumball-java-typical/Main.java
+++ lab-1/gumball-java-typical/Main.java
@@ -19,6 +19,7 @@ public class Main {
 		System.out.println(gumballMachine);
 
 
+        System.out.println("\n\nTesting Gumball Machine 2");
 		GumballMachineType2 gbm2 = new GumballMachineType2(3);
 
         System.out.println("\nScenario 1 where only 25 cents are inserted");
@@ -50,6 +51,42 @@ public class Main {
         gbm2.insertCoin( 25 );
         gbm2.turnCrank();
 
+
+        System.out.println("\n\nTesting Gumball Machine 3");
+        GumballMachineType3 gbm3 = new GumballMachineType3(2);
+
+        System.out.println("\n\nScenario 1 where only 25 cents are inserted");
+        gbm3.insertCoin( 25 );
+        gbm3.turnCrank();
+
+        System.out.println("\nScenario 2 where remaining 25 cents are inserted");
+        gbm3.insertCoin( 25 );
+        gbm3.turnCrank();
+
+        System.out.println("\nScenario 3 where 3x nickle 1x dime and 1x Quarter inserted");
+        gbm3.insertCoin( 5 );
+        gbm3.insertCoin( 5 );
+        gbm3.insertCoin( 5 );
+        gbm3.insertCoin( 10 );
+        gbm3.insertCoin( 25 );
+
+        System.out.println("\nScenario 4 where total amount is more than 50.");
+        gbm3.insertCoin( 25 );
+        gbm3.insertCoin( 10 );
+        gbm3.insertCoin( 5 );
+        gbm3.insertCoin( 5 );
+        gbm3.turnCrank();
+
+        System.out.println("\nScenario 5 where partial amount is paid");
+        gbm3.insertCoin( 25 );
+        gbm3.insertCoin( 10 );
+        gbm3.insertCoin( 5 );
+        gbm3.turnCrank();
+
+        System.out.println("\nScenario 6 where all the Gumballs are used.");
+        gbm3.insertCoin( 25 );
+        gbm3.insertCoin( 25 );
+        gbm3.turnCrank();
 
 
     }
